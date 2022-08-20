2551c15da4720f22693c139f6ed658b63ca1b673
Joaquin Rapela <joacorapela@gmail.com>
2022-08-20 01:21:14 +0100

    Simple modifications

--- code/scripts/doUnpackResultsForPython.m
+++ code/scripts/doUnpackResultsForPython.m
@@ -1,8 +1,8 @@
 clear all;
 
 session_id = 256511;
-packed_data_filename_pattern = "../../../data/risky_choice_data/risky_%d.mat";
-unpacked_data_filename_pattern = "../../../data/risky_choice_data/risky_%d_unpacked_for_python.mat";
+packed_data_filename_pattern = "../../data/risky_choice_data/risky_%d.mat";
+unpacked_data_filename_pattern = "../../data/risky_choice_data/risky_%d_unpacked_for_python.mat";
 
 packed_data_filename = sprintf(packed_data_filename_pattern, session_id);
 unpacked_data_filename = sprintf(unpacked_data_filename_pattern, session_id);
@@ -10,6 +10,7 @@ unpacked_data_filename = sprintf(unpacked_data_filename_pattern, session_id);
 load(packed_data_filename);
 
 clusters_labels = CD.info.('label');
+cell_id = CD.info.('cellid');
 rt_choice = PD.('RT_choice');
 rt_fixation = PD.('RT_fixation');
 
