51be1f2a56d90d127e1b03b07ab2982f03519614
Joaquin Rapela <joacorapela@gmail.com>
2022-10-22 16:32:53 +0100

    Added separateNeuronsSpikeTimesByTrials and other changes

--- scripts/doEpochSpikesTimes.py
+++ scripts/doEpochSpikesTimes.py
@@ -2,28 +2,9 @@
 import sys
 import argparse
 import pickle
-import numpy as np
 import scipy.io
 
-
-def separateNeuronsSpikeTimesByTrials(neurons_spike_times, epochs_times,
-                                      epochs_start_times, epochs_end_times):
-    n_trials = len(epochs_times)
-    n_neurons = len(neurons_spike_times)
-    trials_spikes_times = [[] for r in range(n_trials)]
-    for r in range(n_trials):
-        trial_epoch_time = epochs_times[r]
-        trial_start_time = epochs_start_times[r]
-        trial_end_time = epochs_end_times[r]
-        trial_spikes_times = [[] for n in range(n_neurons)]
-        for n in range(n_neurons):
-            neuron_spikes_times = neurons_spike_times[n]
-            trial_neuron_spikes_times = neuron_spikes_times[
-                np.logical_and(trial_start_time <= neuron_spikes_times,
-                               neuron_spikes_times < trial_end_time)]
-            trial_spikes_times[n] = trial_neuron_spikes_times-trial_epoch_time
-        trials_spikes_times[r] = trial_spikes_times
-    return trials_spikes_times
+import svGPFA.utils.miscUtils
 
 
 def main(argv):
@@ -43,7 +24,7 @@ def main(argv):
     parser.add_argument("--center_choice_var_name",
                         help="center choice variable namen",
                         type=str, default="cue_t")
-    parser.add_argument("--side_choice_var_name",
+    parser.add_argument("--choice_poke_var_name",
                         help="side choice variable namen",
                         type=str, default="target_t")
     parser.add_argument("--rt_choice_var_name",
@@ -76,15 +57,15 @@ def main(argv):
     parser.add_argument("--spike_data_filename_pattern",
                         help="spike data filename pattern",
                         type=str,
-                        default="../../data/risky_choice_data/risky_{:d}_unpacked_for_python.mat")
+                        default="../data/risky_choice_data/risky_{:d}_unpacked_for_python.mat")
     parser.add_argument("--exp_data_filename_pattern",
                         help="experiment data filename pattern",
                         type=str,
-                        default="../../data/risky_{:d}_add_unpacked_for_python.mat")
+                        default="../data/risky_{:d}_add_unpacked_for_python.mat")
     parser.add_argument("--epoched_spikes_filename_pattern",
                         help="filename containing the epoched spikes",
                         type=str,
-                        default="../../results/risky_{:d}_epoched_spikes.pickle")
+                        default="../results/risky_{:d}_epoched_spikes.pickle")
     args = parser.parse_args()
 
     session_id = args.session_id
@@ -92,7 +73,7 @@ def main(argv):
     cell_id_var_name = args.cell_id_var_name
     spikes_times_var_name = args.spikes_times_var_name
     center_choice_var_name = args.center_choice_var_name
-    side_choice_var_name = args.side_choice_var_name
+    choice_poke_var_name = args.choice_poke_var_name
     rt_choice_var_name = args.rt_choice_var_name
     rt_fixation_var_name = args.rt_fixation_var_name
     clusters_labels_var_name = args.clusters_labels_var_name
@@ -120,14 +101,16 @@ def main(argv):
 
     exp_data = scipy.io.loadmat(exp_data_filename)
     center_choice_times = exp_data[center_choice_var_name]
-    side_choice_times = exp_data[side_choice_var_name]
+    choice_poke_times = exp_data[choice_poke_var_name]
     rt_fixation = exp_data[rt_fixation_var_name]
     rt_choice = exp_data[rt_choice_var_name]
     lottery_magnitude = exp_data[lottery_magnitude_var_name]
     surebet_magnitude = exp_data[surebet_magnitude_var_name]
-    subject_choice = exp_data[subject_choice_var_name]
+    subject_choices = exp_data[subject_choice_var_name]
+    subject_choices = [a_subject_choice.item().item()
+                       for a_subject_choice in subject_choices]
     current_reward = exp_data[current_reward_var_name]
-    side_on_times = side_choice_times - rt_choice
+    choice_on_times = choice_poke_times - rt_choice
 
     valid_clusters_indices = [i for i in range(len(clusters_labels))
                               if clusters_labels[i] in clusters_types]
@@ -135,33 +118,32 @@ def main(argv):
     valid_cell_ids = cell_ids[valid_clusters_indices]
 
     epochs_times = center_choice_times
-    epochs_start_times_abs = center_choice_times - rt_fixation - \
+    trials_start_times_abs = center_choice_times - rt_fixation - \
         epoch_start_offset
-    epochs_end_times_abs = side_choice_times + rt_choice + epoch_end_offset
-
-    epoched_spikes_times = separateNeuronsSpikeTimesByTrials(
+    trials_end_times_abs = choice_poke_times + rt_choice + epoch_end_offset
+    epoched_spikes_times = svGPFA.miscUtils.separateNeuronsSpikeTimesByTrials(
         neurons_spike_times=valid_spikes_times,
         epochs_times=epochs_times,
-        epochs_start_times=epochs_start_times_abs,
-        epochs_end_times=epochs_end_times_abs)
-    epochs_start_times_rel = epochs_start_times_abs - epochs_times
-    epochs_end_times_rel = epochs_end_times_abs - epochs_times
+        trials_start_times=trials_start_times_abs,
+        trials_end_times=trials_end_times_abs)
+    trials_start_times_rel = trials_start_times_abs - epochs_times
+    trials_end_times_rel = trials_end_times_abs - epochs_times
     center_on_times_abs = center_choice_times - rt_fixation
     center_on_times_rel = center_on_times_abs - epochs_times
-    side_on_times_rel = side_on_times - epochs_times
-    side_choice_times_rel = side_choice_times - epochs_times
+    choice_on_times_rel = choice_on_times - epochs_times
+    choice_poke_times_rel = choice_poke_times - epochs_times
     results_to_save = {"spikes_times": epoched_spikes_times,
-                       "epochs_start_times": epochs_start_times_rel,
-                       "epochs_end_times": epochs_end_times_rel,
+                       "trials_start_times": trials_start_times_rel,
+                       "trials_end_times": trials_end_times_rel,
                        "center_on_times": center_on_times_rel,
-                       "side_on_times": side_on_times_rel,
-                       "side_choice_times": side_choice_times_rel,
+                       "choice_on_times": choice_on_times_rel,
+                       "choice_poke_times": choice_poke_times_rel,
                        "rt_choice": rt_choice,
                        "rt_fixation": rt_fixation,
                        "cell_ids": valid_cell_ids,
                        "lottery_magnitude": lottery_magnitude,
                        "surebet_magnitude": surebet_magnitude,
-                       "subject_choice": subject_choice,
+                       "subject_choices": subject_choices,
                        "current_reward": current_reward}
     with open(epoched_spikes_filename, "wb") as f:
         pickle.dump(results_to_save, f)
